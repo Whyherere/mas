d0c0e81d81f43aaf62c8d25348575f73b9d1183f
Whyhere <148620619+Whyherere@users.noreply.github.com>
2024-04-26 21:23:17 +0300

    Add files via upload

---
+++ mas/mas.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+  /* 1  $arr = [1, 2, 3, 4, 5,6];
+    $sum = 0;
+    for ($i = 0; $i < count($arr)/2; $i++) {
+        $sum += $arr[$i];
+    }
+echo $sum; */
+/* 2 $arr = [1, 2, 3, 4, 5,6];
+$half = count($arr) / 2;
+$first = 0;
+for ($i = 0; $i < $half; $i++) {
+    $first += $arr[$i];
+}
+$second = 0;
+for ($i = $half; $i < count($arr); $i++) {
+    $second += $arr[$i];
+}
+    $result = $first / $second;
+    echo $result;
+ */
+
+
+    ?>
+</body>
+</html>
